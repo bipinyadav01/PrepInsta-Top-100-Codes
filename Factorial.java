6a43910ac1724fed65894971d67a2f4ff8a78086
Bipin <143310193+bipinyadav01@users.noreply.github.com>
2026-05-21 11:17:40 +0530

    Add Factorial class to calculate factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 3;
+        int fact = 1;
+
+        
+        for (int i = n; i >= 1; i--) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + n + " is: " + fact);
+    }
+}
